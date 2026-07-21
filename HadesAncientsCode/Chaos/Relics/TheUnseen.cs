c40d89108443044b4ef51f38a5eab32c9931ac5e
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-07-21 18:08:50 +0200

    Implement 4 Chaos relics.

---
+++ HadesAncientsCode/Chaos/Relics/TheUnseen.cs
@@ -0,0 +1,97 @@
+﻿using BaseLib.Utils;
+using HadesAncients.HadesAncientsCode.Chaos.Relics.Types;
+using HadesAncients.HadesAncientsCode.Shared.Abstracts;
+using HadesAncients.HadesAncientsCode.Shared.Enums;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Saves.Runs;
+
+namespace HadesAncients.HadesAncientsCode.Chaos.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class TheUnseen() : HadesAncientsRelic(HadesAncient.Chaos), IArcanaRelic
+{
+    private const string TurnsKey = "Turns";
+    private bool _isActivating;
+    private int _turnsSeen;
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(2),
+        new(TurnsKey, 4M)
+    ];
+
+    public override bool ShowCounter => true;
+
+    public override int DisplayAmount => !IsActivating ? TurnsSeen : DynamicVars[TurnsKey].IntValue;
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.ForEnergy(this)];
+
+
+    public bool IsActivating
+    {
+        get => _isActivating;
+        set
+        {
+            AssertMutable();
+            _isActivating = value;
+            InvokeDisplayAmountChanged();
+        }
+    }
+
+    [SavedProperty]
+    public int TurnsSeen
+    {
+        get => _turnsSeen;
+        set
+        {
+            AssertMutable();
+            _turnsSeen = value;
+            InvokeDisplayAmountChanged();
+        }
+    }
+
+    public int GetArcanaRelicNumber()
+    {
+        return 9;
+    }
+
+    public override async Task AfterSideTurnStart(
+        CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState)
+    {
+        if (!participants.Contains(Owner.Creature))
+            return;
+        TurnsSeen = (TurnsSeen + 1) % DynamicVars[TurnsKey].IntValue;
+        Status = TurnsSeen == DynamicVars[TurnsKey].IntValue - 1
+            ? RelicStatus.Active
+            : RelicStatus.Normal;
+        if (TurnsSeen != 0)
+            return;
+        _ = TaskHelper.RunSafely(DoActivateVisuals());
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
+    }
+
+    private async Task DoActivateVisuals()
+    {
+        IsActivating = true;
+        Flash();
+        await Cmd.Wait(1f);
+        IsActivating = false;
+    }
+
+    public override Task AfterCombatEnd(CombatRoom _)
+    {
+        Status = RelicStatus.Normal;
+        return Task.CompletedTask;
+    }
+}
