97955a1d4a6c3e3040a6cdafe8a8c7969491e80c
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-08-08 19:11:58 +0200

    Add the structure for Ares and 'Visceral Impact' relic.

---
+++ HadesAncientsCode/Ares/Ancients/AresAncient.cs
@@ -0,0 +1,43 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using Godot;
+using HadesAncients.HadesAncientsCode.Ares.Relics;
+using HadesAncients.HadesAncientsCode.Shared.Enums;
+using HadesAncients.HadesAncientsCode.Shared.Extensions;
+using MegaCrit.Sts2.Core.Models;
+
+namespace HadesAncients.HadesAncientsCode.Ares.Ancients;
+
+[Pool(typeof(AncientEventModel))]
+public class AresAncient : CustomAncientModel
+{
+    public Vector2 ChooseTheAncientPortalExtraOffset => new(650f, 110f);
+
+    public override string CustomScenePath => "dionysus.tscn".AncientImagePath(HadesAncient.Ares);
+    public override string CustomMapIconPath => "map_icon.png".AncientImagePath(HadesAncient.Ares);
+    public override string CustomMapIconOutlinePath => "map_icon_outline.png".AncientImagePath(HadesAncient.Ares);
+    public override string CustomRunHistoryIconPath => "run_history_icon.png".AncientImagePath(HadesAncient.Ares);
+    public override string CustomRunHistoryIconOutlinePath =>
+        "run_history_icon_outline.png".AncientImagePath(HadesAncient.Ares);
+
+    protected override OptionPools MakeOptionPools
+    {
+        get
+        {
+            List<AncientOption> relics =
+            [
+                AncientOption<VisceralImpact>(),
+            ];
+
+            return new OptionPools(
+                MakePool(relics.ToArray())
+            );
+        }
+    }
+
+    public override bool IsValidForAct(ActModel act)
+    {
+        return act.ActNumber() == 3 && !HadesAncientsModConfig.DisableAres;
+    }
+}
---
+++ HadesAncientsCode/Ares/Cards/ShiftingForm.cs
@@ -0,0 +1,33 @@
+﻿using BaseLib.Utils;
+using HadesAncients.HadesAncientsCode.Shared.Abstracts;
+using HadesAncients.HadesAncientsCode.Shared.Enums;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace HadesAncients.HadesAncientsCode.Ares.Cards;
+
+[Pool(typeof(EventCardPool))]
+public class ShiftingForm() : HadesAncientsCard(HadesAncient.Ares, 3, CardType.Power, CardRarity.Token, TargetType.Self)
+{
+    public override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ICombatState combatState = cardPlay.Player.Creature.CombatState!;
+
+        DemonForm demonForm = combatState.CreateCard<DemonForm>(Owner);
+        SerpentForm serpentForm = combatState.CreateCard<SerpentForm>(Owner);
+        VoidForm voidForm = combatState.CreateCard<VoidForm>(Owner);
+        ReaperForm reaperForm = combatState.CreateCard<ReaperForm>(Owner);
+        EchoForm echoForm = combatState.CreateCard<EchoForm>(Owner);
+
+        List<CardModel> formCards = [demonForm, serpentForm, voidForm, reaperForm, echoForm];
+
+        Owner.Creature.CombatState!.RunState.Rng.Niche.Shuffle(formCards);
+        
+        await CardCmd.AutoPlay(choiceContext, formCards[0], null);
+    }
+}
