011d960de2375ef2e1be153207d61e660ff164de
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-08-29 18:38:25 +0200

    Small refactor of Sea Star.

---
+++ HadesAncientsCode/Shared/Utils/RewardUtils.cs
@@ -0,0 +1,44 @@
+﻿using Godot;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Nodes.Rewards;
+using MegaCrit.Sts2.Core.Nodes.Screens;
+using MegaCrit.Sts2.Core.Nodes.Screens.Overlays;
+using MegaCrit.Sts2.Core.Rewards;
+
+namespace HadesAncients.HadesAncientsCode.Shared.Utils;
+
+public class RewardUtils
+{
+    public static void AddRewardToCurrentScreen(Reward newReward)
+    {
+        newReward.MarkContentAsSeen();
+
+        NRewardsScreen? screen = NOverlayStack.Instance?
+            .GetChildren()
+            .OfType<NRewardsScreen>()
+            .LastOrDefault();
+
+        if (screen == null)
+        {
+            return;
+        }
+
+        var button = NRewardButton.Create(newReward, screen);
+
+        button.Connect(
+            NRewardButton.SignalName.RewardClaimed,
+            Callable.From<NRewardButton>(screen.RewardCollectedFrom)
+        );
+
+        button.Connect(
+            NRewardButton.SignalName.RewardSkipped,
+            Callable.From<NRewardButton>(screen.RewardSkippedFrom)
+        );
+
+        screen._rewardButtons.Add(button);
+        screen._rewardsContainer.AddChildSafely(button);
+
+        screen.UpdateScreenState();
+        screen.TryEnableProceedButton();
+    }
+}
