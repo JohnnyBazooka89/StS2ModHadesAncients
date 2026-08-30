ff37484ee590b9e204cb269a83f38c8792cf5a8b
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-08-30 20:34:28 +0200

    More relics.

--- HadesAncientsCode/Athena/Relics/PhalanxShot.cs
+++ HadesAncientsCode/Athena/Relics/PhalanxShot.cs
@@ -1,37 +1,97 @@
 ﻿using BaseLib.Utils;
 using HadesAncients.HadesAncientsCode.Shared.Abstracts;
 using HadesAncients.HadesAncientsCode.Shared.Enums;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace HadesAncients.HadesAncientsCode.Athena.Relics;
 
 [Pool(typeof(EventRelicPool))]
 public class PhalanxShot() : HadesAncientsRelic(HadesAncient.Athena)
 {
+    private const string BlockPercentKey = "BlockPercent";
+
     public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override bool ShowCounter => CombatManager.Instance.IsInProgress;
+
+    public override int DisplayAmount => (int)GetDamageToDeal();
 
     public override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new PowerVar<VigorPower>(3M)
+        new(BlockPercentKey, 50M)
     ];
 
-    public override IEnumerable<IHoverTip> ExtraHoverTips =>
-    [
-        HoverTipFactory.FromPower<VigorPower>()
-    ];
-
-    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    private decimal GetDamageToDeal()
     {
-        if (cardPlay.Card.Type != CardType.Skill || cardPlay.Card.Owner != Owner)
+        return Owner.Creature.Block * DynamicVars[BlockPercentKey].BaseValue / 100M;
+    }
+
+    public override async Task BeforeSideTurnEnd(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IEnumerable<Creature> participants)
+    {
+        decimal damageToDeal = GetDamageToDeal();
+        if (!participants.Contains(Owner.Creature) || damageToDeal <= 0)
             return;
-        await PowerCmd.Apply<VigorPower>(choiceContext, Owner.Creature, DynamicVars[nameof(VigorPower)].BaseValue,
-            Owner.Creature, null);
+
+        Flash();
+
+        foreach (Creature hittableEnemy in Owner.Creature.CombatState!.HittableEnemies)
+        {
+            VfxCmd.PlayOnCreature(hittableEnemy, VfxCmd.slashPath);
+        }
+
+        await CreatureCmd.Damage(new ThrowingPlayerChoiceContext(), Owner.Creature.CombatState!.HittableEnemies,
+            damageToDeal, ValueProp.Unpowered, Owner.Creature);
+    }
+
+    public override Task AfterBlockGained(Creature creature, decimal amount, ValueProp props, CardModel? cardSource)
+    {
+        if (creature != Owner.Creature)
+        {
+            return Task.CompletedTask;
+        }
+
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterBlockCleared(Creature creature)
+    {
+        if (creature != Owner.Creature)
+        {
+            return Task.CompletedTask;
+        }
+
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
+    }
+
+    public override Task BeforeSideTurnStart(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState)
+    {
+        if (!participants.Contains(Owner.Creature))
+            return Task.CompletedTask;
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterCombatEnd(CombatRoom _)
+    {
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
     }
 }
---
+++ HadesAncientsCode/Hestia/Relics/HeatRush.cs
@@ -0,0 +1,51 @@
+﻿using BaseLib.Utils;
+using HadesAncients.HadesAncientsCode.Shared.Abstracts;
+using HadesAncients.HadesAncientsCode.Shared.Compatibility;
+using HadesAncients.HadesAncientsCode.Shared.Enums;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace HadesAncients.HadesAncientsCode.Hestia.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class HeatRush() : HadesAncientsRelic(HadesAncient.Hestia)
+{
+    private const string CardsToExhaustKey = "CardsToExhaust";
+
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1),
+        new(CardsToExhaustKey, 1)
+    ];
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.ForEnergy(this),
+        HoverTipFactory.FromKeyword(CardKeyword.Exhaust)
+    ];
+
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        return player != Owner ? amount : amount + DynamicVars.Energy.IntValue;
+    }
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner)
+            return;
+        foreach (CardModel card in await CardSelectCmd.FromHand(choiceContext, player,
+                     new CardSelectorPrefs(CardSelectorPrefs.ExhaustSelectionPrompt,
+                         DynamicVars[CardsToExhaustKey].IntValue), null, this))
+            await CardCmdCompatibility.Exhaust(choiceContext, card);
+    }
+}
