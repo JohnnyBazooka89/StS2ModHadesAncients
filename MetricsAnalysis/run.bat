b04d4ea82aeecd297d7e10197972805b110c7f3e
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-09-20 19:47:51 +0200

    Add Metrics Analysis scripts to the repo.

---
+++ MetricsAnalysis/run.bat
@@ -0,0 +1,3 @@
+python3 metrics-hadesancients.py --METRICS_PATH "D:\metrics_runs\hades_ancients_metrics\runs" --DATABASE hades_ancients --DATABASE_USER postgres --DATABASE_PASSWORD secret --FIND_NEW_RUNS_TO_PROCESS true --PROCESS_RUNS true
+
+python3 metrics-hadesancients-generate-graphs.py
