2a8d990a0f27f2bfd1b334f3d6746fde9b20712e
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-09-20 11:19:09 +0200

    Add metrics.

---
+++ HadesAncientsCode/Data/ModManagerCallMetricsHooksPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Saves;
+
+namespace HadesAncients.HadesAncientsCode.Data;
+
+/**
+ * There is a problem with ModManager.OnMetricsUpload += HadesAncientsMetrics.OnMetricsUpload;
+ * because of Publicizer, so this patch is a workaround.
+ */
+[HarmonyPatch(typeof(ModManager), nameof(ModManager.CallMetricsHooks))]
+public static class ModManagerCallMetricsHooksPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(
+        SerializableRun run,
+        bool isVictory,
+        ulong localPlayerId)
+    {
+        HadesAncientsMetrics.OnMetricsUpload(
+            run,
+            isVictory,
+            localPlayerId
+        );
+    }
+}
